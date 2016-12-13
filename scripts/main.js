3f28155202fd728973a5712f02b4ca2008852e15
Dakota Bryant <bryant.dakota@gmail.com>
2016-12-13 00:25:12 -0600

    Keep grinding. Contact page, etc

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,18 @@
-$(function() {
+$(document).ready(function() {
 	$('.nav-toggle').on('click', function() {
 		$('.main-nav').toggleClass('open');
 	});
+	$('.contact-toggle').on('click', function() {
+		$('.contact').toggleClass('open');
+	});
+	$('#spinner').bind('ajxSend', function() {
+			$(this).show();
+	}).bind('ajaxStop', function() {
+		$(this).hide();
+	}).bind('ajaxError', function() {
+		$(this).hide();
+
+	});
+
 
 });
