789c70b26cbcd990bab2baad772fd995c938e9e4
Dakota Bryant <bryant.dakota@gmail.com>
2018-05-21 01:10:49 -0400

    started on portfolio

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,33 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Dakota Bryant'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
-}
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass', {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages'
+      }
+    }, {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: './src/favicon.png',
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
+    },
+    'gatsby-transformer-remark'
+  ]
+};
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,36 +5,39 @@ import Helmet from 'react-helmet'
 import Header from '../components/header'
 import './index.css'
 
-const Layout = ({ children, data }) => (
+const Layout = ({children, data}) => (
   <div>
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header siteTitle={data.site.siteMetadata.title} />
+      {
+        name: 'description',
+        content: 'Sample'
+      }, {
+        name: 'keywords',
+        content: 'sample, something'
+      }
+    ]}/>
+    <Header siteTitle={data.site.siteMetadata.title}/>
     <div
       style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+      margin: '0 auto',
+      maxWidth: 960,
+      padding: '0px 1.0875rem 1.45rem',
+      paddingTop: 0
+    }}>
       {children()}
     </div>
   </div>
 )
 
 Layout.propTypes = {
-  children: PropTypes.func,
+  children: PropTypes.func
 }
 
 export default Layout
 
-export const query = graphql`
+export const query = graphql `
   query SiteTitleQuery {
     site {
       siteMetadata {
