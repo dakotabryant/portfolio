69508567bec9252b606356f5c4116fdc55f064db
Dakota Bryant <bryant.dakota@gmail.com>
2018-09-04 23:05:55 -0400

    trying form

--- src/components/ContactModal.js
+++ src/components/ContactModal.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { push } from 'gatsby';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import { toggleModal } from '../actions/main';
+import { connect } from 'react-redux';
+
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
+};
 
 class ContactModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      message: '',
+    };
+  }
   componentWillReceiveProps(nextProps) {
     const bodySelector = document.getElementsByTagName('body')[0];
     if (nextProps.showModal) {
@@ -11,31 +28,55 @@ class ContactModal extends React.Component {
       bodySelector.classList.remove('no-scroll');
     }
   }
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  handleSubmit = e => {
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...this.state }),
+    })
+      .then(() => this.props.dispatch(toggleModal()))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
   render() {
     const { showModal, onClose } = this.props;
+    const { name, email, message } = this.state;
     if (showModal) {
       return (
         <div className="contact-modal">
           {showModal && (
-            <form
-              name="contact"
-              data-netlify="true"
-              method="POST"
-              className="contact-form"
-            >
+            <form className="contact-form" onSubmit={this.handleSubmit}>
+              <input type="hidden" name="form-name" value="contact" />
               <FontAwesomeIcon
                 icon={faTimesCircle}
                 className="close-button"
                 onClick={onClose}
               />
-              <input type="hidden" name="form-name" value="contact" />
 
               <label htmlFor="name">Name</label>
-              <input type="text" name="name" />
+              <input
+                onChange={this.handleChange}
+                type="text"
+                name="name"
+                value={name}
+              />
               <label htmlFor="email">Email</label>
-              <input type="text" name="email" />
+              <input
+                onChange={this.handleChange}
+                type="text"
+                name="email"
+                value={email}
+              />
               <label htmlFor="message">Message</label>
-              <textarea name="message" />
+              <textarea
+                name="message"
+                onChange={this.handleChange}
+                value={message}
+              />
               <button type="submit">Send</button>
             </form>
           )}
@@ -45,4 +86,4 @@ class ContactModal extends React.Component {
   }
 }
 
-export default ContactModal;
+export default connect()(ContactModal);
