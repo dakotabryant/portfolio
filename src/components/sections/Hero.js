613a2d30573a86646fe3118535d2a6da38de0d3c
Dakota Bryant <bryant.dakota@gmail.com>
2018-10-07 23:32:45 -0400

    fixed image size, favicon, etc

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Name from '../../assets/titles/db.svg';
+import Name from '../../assets/titles/db.png';
 import { toggleModal } from '../../actions/main';
 import { connect } from 'react-redux';
 class Hero extends Component {
   render() {
     return (
       <section className="hero">
-        <object data={Name} type="image/svg+xml" aria-label="hero" />
+        <img src={Name} alt="" />
       </section>
     );
   }
